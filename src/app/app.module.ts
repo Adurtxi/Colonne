9b686493b9c97dd098927eb3de2e7a92b6a657e7
Adur Marques <adur.marques@gmail.com>
2020-05-26 01:43:03 +0200

    Packages updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { CountdownModule } from 'ngx-countdown';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,7 +22,6 @@ import { ColorPickerComponent } from './components/imports/color-picker/color-pi
     AppRoutingModule,
     NgbModule,
     FormsModule,
-    CountdownModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
